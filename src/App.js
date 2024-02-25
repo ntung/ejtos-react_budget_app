f4039f5d9aedd6ab923839527b3538a463900519
Tung Nguyen <nvntung@gmail.com>
2024-02-25 11:29:26 +0000

    Step 02: Make changes to the  Budget component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//Code to import Budget.js
 import Budget from './components/Budget';
 
-// Add code to import the other components here under
 
 
 import { AppProvider } from './context/AppContext';
@@ -15,9 +13,9 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                        <div className='col-sm'>
+                            <Budget />
+                        </div> 
 
                         {
                             /* Add Remaining component here*/
